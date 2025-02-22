c5b7c094192ebb13c098d9923e718a524171caa6
Ibragim <lol.3007@mail.ru>
2025-02-21 17:07:15 -0800

    update dare displayt design

--- src/module/DareDisplay/ButtomTimerInCard.tsx
+++ src/module/DareDisplay/ButtomTimerInCard.tsx
@@ -90,85 +90,60 @@ export default function ButtomTimerInCard({
     setTimeLeft(dare.time);
   };
 
-  const rotationInterpolated = interpolate(
-    rotation.value,
-    [0, 360],
-    [0, 360],
-    Extrapolate.CLAMP
-  );
-
   return (
-    <Grid>
-      <Grid>
-        {/* <Animated.View style={{ transform: [{ scale: scale }] }}>
-          <Grid row align="center" space="md">
-            <Typography weight="bold" variant="title-0">
-              {generateTimerTime(timeLeft)}
-            </Typography>
-          </Grid>
-        </Animated.View> */}
-        <Grid row align="center" space="md" justfity="center">
-          {timerWasStarted && (
-            <Animated.View entering={FadeInRight}>
-              <WrapIconInCircle onPress={toggleTimer}>
-                <FontAwesome6
-                  name={isTimerRunning ? "pause" : "play"}
-                  size={20}
-                  color={colors.accent.secondary}
-                />
-              </WrapIconInCircle>
-            </Animated.View>
-          )}
+    <Grid row align="center" space="md" justfity="center">
+      {timerWasStarted && (
+        <Animated.View entering={FadeInRight}>
+          <WrapIconInCircle onPress={toggleTimer}>
+            <FontAwesome6
+              name={isTimerRunning ? "pause" : "play"}
+              size={20}
+              color={colors.accent.secondary}
+            />
+          </WrapIconInCircle>
+        </Animated.View>
+      )}
 
-          <Button
-            style={{
-              // width: "100%",
-              borderColor: colors.accent.secondary,
-              borderWidth: 1,
-              paddingHorizontal: 15,
-              paddingVertical: 10,
-              backgroundColor: isTimerRunning
-                ? colors.accent.secondary
-                : "transparent",
-            }}
-            textStyle={{
-              style: {
-                color: isTimerRunning ? "#fff" : colors.accent.secondary,
-                fontFamily: fontWeight.regular,
-              },
-            }}
-            onPress={toggleTimer}
-            startIcon={
-              <Ionicons
-                name="stopwatch-outline"
-                size={28}
-                color={isTimerRunning ? "#fff" : colors.accent.secondary}
-              />
-            }
-            title={generateTimerTime(timeLeft)}
+      <Button
+        style={{
+          // width: "100%",
+          borderColor: colors.accent.secondary,
+          borderWidth: 1,
+          paddingHorizontal: 20,
+          paddingVertical: 18,
+          backgroundColor: isTimerRunning
+            ? colors.accent.secondary
+            : "transparent",
+        }}
+        textStyle={{
+          style: {
+            color: isTimerRunning ? "#fff" : colors.accent.secondary,
+            fontSize: 23,
+            fontFamily: fontWeight.regular,
+          },
+        }}
+        onPress={toggleTimer}
+        startIcon={
+          <Ionicons
+            name="stopwatch-outline"
+            size={32}
+            color={isTimerRunning ? "#fff" : colors.accent.secondary}
           />
+        }
+        title={generateTimerTime(timeLeft)}
+      />
 
-          {timerWasStarted && (
-            <Animated.View entering={FadeInLeft}>
-              <WrapIconInCircle onPress={resetTimer}>
-                <FontAwesome6
-                  name={"arrow-rotate-right"}
-                  size={20}
-                  color={colors.accent.secondary}
-                />
-              </WrapIconInCircle>
-            </Animated.View>
-          )}
-        </Grid>
-      </Grid>
-      {/* <Pressable onPress={skipTimer}>
-        <Typography
-          style={{ textDecorationLine: "underline" }}
-          variant="callout"
-        >
-          Пропустить
-        </Typography>
-      </Pressable> */}
+      {timerWasStarted && (
+        <Animated.View entering={FadeInLeft}>
+          <WrapIconInCircle onPress={resetTimer}>
+            <FontAwesome6
+              name={"arrow-rotate-right"}
+              size={20}
+              color={colors.accent.secondary}
+            />
+          </WrapIconInCircle>
+        </Animated.View>
+      )}
     </Grid>
   );
 }
--- src/module/DareDisplay/DareDisplay.tsx
+++ src/module/DareDisplay/DareDisplay.tsx
@@ -8,14 +8,14 @@ import {
 import { useTheme } from "@/src/shared/hooks/useTheme";
 import { Dare, Player } from "@/src/shared/types/globalTypes";
 import Button from "@/src/shared/ui/buttons/Button";
+import GradientShadow from "@/src/shared/ui/elements/GradientShadow";
 import Grid from "@/src/shared/ui/grid/Grid";
-import Paper from "@/src/shared/ui/layout/Paper";
 import Typography from "@/src/shared/ui/typography/Typography";
 import { normalizedSize } from "@/src/shared/utils/size";
 import { Image } from "expo-image";
+import { useRef, useState } from "react";
+import { View } from "react-native";
 import Animated, { SlideInRight, SlideOutLeft } from "react-native-reanimated";
-import ButtonTimer from "./ButtonTimer";
-import GradientShadow from "@/src/shared/ui/elements/GradientShadow";
 import ButtomTimerInCard from "./ButtomTimerInCard";
 
 interface DareDisplayProps {
@@ -31,96 +31,116 @@ export default function DareDisplay({
   players,
   hideDare,
 }: DareDisplayProps) {
+  const ref = useRef<View | null>(null);
+  const [heightBlock, setHeightBlock] = useState(300);
+
   const colors = useTheme();
   const { pickedPackages } = usePackage();
   const currentPackage = pickedPackages.find((pkg) => pkg.id === dare.package);
 
+  const handleLayout = (event: any) => {
+    const { height } = event.nativeEvent.layout;
+    setHeightBlock(height); // обновляем высоту
+  };
   return (
-    <Animated.View entering={SlideInRight} exiting={SlideOutLeft}>
-      <Grid gap={90}>
-        <Paper
-          marginHorizontal={HORIZONTAL_PADDINGS}
-          padding={30}
-          paddingBottom={40}
+    <Animated.View
+      style={{ width: "100%", marginHorizontal: HORIZONTAL_PADDINGS }}
+      entering={SlideInRight}
+      exiting={SlideOutLeft}
+    >
+      <Grid gap={80} align="center">
+        {/* <Grid
+          color={colors.text.white}
+          width="95%"
+          height={heightBlock}
           style={{
-            backgroundColor: colors.text.white,
+            position: "absolute",
             borderRadius: 40,
+            alignSelf: "center",
+            zIndex: -10,
+            // transform: [
+            //   {
+            //     skewX: "-5deg",
+            //   },
+            //   { scale: 0.96 },
+            //   {
+            //     skewY: "-3deg",
+            //   },
+            // ],
           }}
         >
           <GradientShadow
             secondColor={colors.text.white}
-            height={50}
-            color={colors.accent.primary}
+            height={35}
+            color={colors.background.primary}
           />
+        </Grid> */}
 
-          <Grid align="center" paddingBottom={30} space="md">
-            {dare.time && (
-              <Grid width="100%">
+        <View ref={ref} onLayout={handleLayout}>
+          <Grid
+            paddingVertical={40}
+            paddingHorizontal={30}
+            width="99%"
+            color={colors.text.primary}
+            style={{ borderRadius: 50 }}
+          >
+            <Grid align="center" space="md">
+              {dare.time ? (
                 <ButtomTimerInCard dare={dare} handleDone={hideDare} />
-              </Grid>
-            )}
+              ) : null}
 
-            {/* <Paper
+              <Grid space="md">
+                <Typography
+                  variant="title-3"
+                  weight="bold"
+                  color="secondary"
+                  textAlign="center"
+                >
+                  {dare.title}
+                </Typography>
+
+                <Typography
+                  style={{ lineHeight: 27 }}
+                  textAlign="center"
+                  weight="regular"
+                  color="secondary"
+                >
+                  {updatedArray(
+                    getTransformedArrayOfString(dare.action),
+                    currentTurn,
+                    players
+                  ).join("")}
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid
+              padding={5}
               style={{
                 position: "absolute",
-                bottom: -70,
-                left: "-15%",
-                transform: [{ rotate: "15deg" }],
-                backgroundColor: colors.text.primary,
-                borderRadius: 100,
+                bottom: -30,
+                right: 15,
+                // backgroundColor: "#fff",
+                borderRadius: 50,
                 shadowColor: colors.background.primary,
-                shadowOpacity: 0.2,
+                shadowOpacity: 0.17,
                 shadowRadius: 10,
-                shadowOffset: {
-                  height: -3,
-                  width: 0,
-                },
               }}
-              padding={5}
             >
               <Image
                 style={{
-                  height: normalizedSize(50),
-                  width: normalizedSize(50),
+                  height: normalizedSize(60),
+                  width: normalizedSize(60),
                 }}
                 source={getActualImageLink(currentPackage?.imageEncoded || "")}
-                contentFit="cover"
+                contentFit="contain"
               />
-            </Paper> */}
-            <Grid space="lg">
-              <Typography
-                variant="title-2"
-                weight="bold"
-                color="secondary"
-                textAlign="center"
-              >
-                {dare.title}
-              </Typography>
-
-              <Typography
-                style={{ lineHeight: 26 }}
-                textAlign="center"
-                weight="medium"
-                color="secondary"
-              >
-                {updatedArray(
-                  getTransformedArrayOfString(dare.action),
-                  currentTurn,
-                  players
-                ).join("")}
-              </Typography>
             </Grid>
           </Grid>
-        </Paper>
-        {/* {dare.time ? (
-          <Grid width="100%" align="center">
-            <ButtonTimer dare={dare} handleDone={hideDare} />
-          </Grid>
-        ) : ( */}
-        <Grid width="100%" align="center">
+        </View>
+
+        <Grid row>
           <Button style={{ width: "80%" }} title="Готово" onPress={hideDare} />
         </Grid>
-        {/* )} */}
       </Grid>
     </Animated.View>
   );
