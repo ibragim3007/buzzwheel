ca4e199bda6073404328b7a3ca68dfea99770634
Ibragim <lol.3007@mail.ru>
2025-02-27 19:59:19 -0800

    Create transformedArrayOfString.test.ts

---
+++ src/test/app.module/transformedArrayOfString.test.ts
@@ -0,0 +1,34 @@
+import { getTransformedArrayOfString } from "@/src/shared/helpers/textConverters/coreLogic";
+
+const str1 = "Hello, ${c_player}! Random player is ${r_player}.";
+const str2 = "Hello, ${prev_player}! Your next player ${next_player}.";
+const str3 = "Random letter is ${r_letter}";
+
+const inStr1 = "Hello, ${c_player! and so one";
+
+describe("getTransformedArrayOfString", () => {
+  test("Correct values", () => {
+    const res1 = getTransformedArrayOfString(str1);
+    const res2 = getTransformedArrayOfString(str2);
+    const res3 = getTransformedArrayOfString(str3);
+
+    const inRes1 = getTransformedArrayOfString(inStr1);
+    expect(res1).toEqual([
+      "Hello, ",
+      "${c_player}",
+      "! Random player is ",
+      "${r_player}",
+      ".",
+    ]);
+    expect(res2).toEqual([
+      "Hello, ",
+      "${prev_player}",
+      "! Your next player ",
+      "${next_player}",
+      ".",
+    ]);
+    expect(res3).toEqual(["Random letter is ", "${r_letter}"]);
+
+    expect(inRes1).toEqual(["Hello, ${c_player! and so one"]);
+  });
+});
