7f1ecc9ac4e4138e3096cdadcb92fa9d9a3c9247
Ibragim <lol.3007@mail.ru>
2025-03-01 20:14:19 -0800

    install linter

--- src/pages/GiftPage/GiftPage.tsx
+++ src/pages/GiftPage/GiftPage.tsx
@@ -1,30 +1,27 @@
-import { convertGiftsToSegments } from "@/src/entities/Gift/convertGiftsToSegments";
-import { useGiftRepositroy } from "@/src/entities/Gift/gift.repository";
-import { mockGifts } from "@/src/entities/Gift/mockGifts";
-import { useModalStore } from "@/src/entities/Modal/useModalStore";
-import { SegmentType } from "@/src/entities/Roulette/types";
-import { giftService } from "@/src/module/GiftModule/GiftService";
-import { Roulette } from "@/src/module/Roulette";
+import { convertGiftsToSegments } from '@/src/entities/Gift/convertGiftsToSegments';
+import { useGiftRepositroy } from '@/src/entities/Gift/gift.repository';
+import { mockGifts } from '@/src/entities/Gift/mockGifts';
+import { useModalStore } from '@/src/entities/Modal/useModalStore';
+import { SegmentType } from '@/src/entities/Roulette/types';
+import { giftService } from '@/src/module/GiftModule/GiftService';
+import { Roulette } from '@/src/module/Roulette';
 
-import { calcByWheelSize } from "@/src/module/Roulette/config/config";
-import {
-  generateSegmentsMock,
-  OPTIONS_SMALL_ROULETTE,
-} from "@/src/module/SettingsGame/RoulettePicker/RoulettePicker";
-import { SettingsConstants } from "@/src/shared/config/constants/settingsOptions";
-import { CustomAnimations } from "@/src/shared/config/theme/AnimationConfig";
+import { calcByWheelSize } from '@/src/module/Roulette/config/config';
+import { generateSegmentsMock, OPTIONS_SMALL_ROULETTE } from '@/src/module/SettingsGame/RoulettePicker/RoulettePicker';
+import { SettingsConstants } from '@/src/shared/config/constants/settingsOptions';
+import { CustomAnimations } from '@/src/shared/config/theme/AnimationConfig';
 
-import { useTheme } from "@/src/shared/hooks/useTheme";
-import Button from "@/src/shared/ui/buttons/Button";
-import SmallAppThemeElem from "@/src/shared/ui/elements/SmallAppThemeElem";
-import SmallRoulette from "@/src/shared/ui/elements/SmallRoulette";
-import Grid from "@/src/shared/ui/grid/Grid";
-import PageWrapper from "@/src/shared/ui/layout/PageWrapper";
-import SafeWrapper from "@/src/shared/ui/layout/SafeWrapper";
-import Typography from "@/src/shared/ui/typography/Typography";
-import Header from "@/src/widget/Header";
-import React, { useEffect, useRef, useState } from "react";
-import Animated, { ZoomIn, ZoomOut } from "react-native-reanimated";
+import { useTheme } from '@/src/shared/hooks/useTheme';
+import Button from '@/src/shared/ui/buttons/Button';
+import SmallAppThemeElem from '@/src/shared/ui/elements/SmallAppThemeElem';
+import SmallRoulette from '@/src/shared/ui/elements/SmallRoulette';
+import Grid from '@/src/shared/ui/grid/Grid';
+import PageWrapper from '@/src/shared/ui/layout/PageWrapper';
+import SafeWrapper from '@/src/shared/ui/layout/SafeWrapper';
+import Typography from '@/src/shared/ui/typography/Typography';
+import Header from '@/src/widget/Header';
+import React, { useEffect, useRef, useState } from 'react';
+import Animated, { ZoomIn, ZoomOut } from 'react-native-reanimated';
 
 // Игкрок зашел первый день и видит получение приза
 // игрок получает возможность крутить колесо и получить фичу
@@ -110,43 +107,37 @@ export default function GiftPage() {
   } = useGiftRepositroy();
 
   const onCallback = (segment: SegmentType) => {
-    if (segment.type === "gift") {
-      const gift = mockGifts.find((gift) => gift.id === segment.id);
+    if (segment.type === 'gift') {
+      const gift = mockGifts.find(gift => gift.id === segment.id);
 
-      if (gift?.type === "app-theme") {
-        const lockedTheme = giftService.getRandomLockedGift(
-          SettingsConstants.themes,
-          unlockedThemes
-        );
+      if (gift?.type === 'app-theme') {
+        const lockedTheme = giftService.getRandomLockedGift(SettingsConstants.themes, unlockedThemes);
         if (lockedTheme) {
           unlockTheme(lockedTheme.id);
           openModal({
-            title: "Вы выйграли подарок!",
-            description: "Вы получили тему для приложения!",
+            title: 'Вы выйграли подарок!',
+            description: 'Вы получили тему для приложения!',
             callbackOnClose: () => setIsAvailableToSpin(false),
             node: <SmallAppThemeElem theme={lockedTheme} />,
-            buttonText: "Получить",
+            buttonText: 'Получить',
           });
         }
       }
-      if (gift?.type === "roulette-colors") {
+      if (gift?.type === 'roulette-colors') {
         const lockedRoulette = giftService.getRandomLockedGift(
           SettingsConstants.availableColors,
-          unlockedRouletteColors
+          unlockedRouletteColors,
         );
         if (lockedRoulette) {
           unlockRouletteColor(lockedRoulette.id);
           openModal({
-            title: "Вы выйграли подарок!",
-            description: "Вы получили колесо для игры!",
+            title: 'Вы выйграли подарок!',
+            description: 'Вы получили колесо для игры!',
             callbackOnClose: () => setIsAvailableToSpin(false),
             node: (
-              <SmallRoulette
-                segments={generateSegmentsMock(lockedRoulette.colors)}
-                options={OPTIONS_SMALL_ROULETTE}
-              />
+              <SmallRoulette segments={generateSegmentsMock(lockedRoulette.colors)} options={OPTIONS_SMALL_ROULETTE} />
             ),
-            buttonText: "Получить",
+            buttonText: 'Получить',
           });
         }
       }
--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,22 +1,20 @@
-import Grid from "@/src/shared/ui/grid/Grid";
-
-import { AddPlayers } from "@/src/module/AddPlayers";
-import SafeWrapper from "@/src/shared/ui/layout/SafeWrapper";
-import Typography from "@/src/shared/ui/typography/Typography";
-import Header from "@/src/widget/Header";
-import React from "react";
-import { usePlayerStore } from "@/src/entities/Player/player.store";
-import { MAX_PLAYERS_FOR_FREE } from "@/src/shared/config/constants/constants";
-import PageWrapper from "@/src/shared/ui/layout/PageWrapper";
+import Grid from '@/src/shared/ui/grid/Grid';
+import { usePlayerStore } from '@/src/entities/Player/player.store';
+import { AddPlayers } from '@/src/module/AddPlayers';
+import PageWrapper from '@/src/shared/ui/layout/PageWrapper';
+import SafeWrapper from '@/src/shared/ui/layout/SafeWrapper';
+import Header from '@/src/widget/Header';
+import React from 'react';
 
 export default function HomePage() {
   const { players } = usePlayerStore();
+  const asd = 'asd';
   return (
     <PageWrapper>
       <SafeWrapper>
         <Header />
         <Grid height="95%" space="lg">
-          <Grid space="md"></Grid>
+          {/* <Grid space="md"></Grid> */}
 
           <AddPlayers />
         </Grid>
