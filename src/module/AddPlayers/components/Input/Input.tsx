79149e1485d9508fbd59c5d2fa76cd2755c2cf3b
Ibragim <lol.3007@mail.ru>
2025-03-24 22:07:57 -0700

    add: animation to error handle input

--- src/module/AddPlayers/components/Input/Input.tsx
+++ src/module/AddPlayers/components/Input/Input.tsx
@@ -1,23 +1,50 @@
 import { useTheme } from '@/src/shared/hooks/useTheme';
+import { useVibration } from '@/src/shared/hooks/useVibration';
 import { fontWeight } from '@/src/shared/styles/typography/typography';
 import Button from '@/src/shared/ui/buttons/Button';
 import Grid from '@/src/shared/ui/grid/Grid';
 import Paper from '@/src/shared/ui/layout/Paper';
 import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 import { useRef, useState } from 'react';
-import { Alert, TextInput } from 'react-native';
+import { TextInput } from 'react-native';
+import Animated, { useAnimatedStyle, useSharedValue, withSpring } from 'react-native-reanimated';
+
 interface InputProps {
   onCall: (name: string) => void;
 }
 
 export default function Input({ onCall }: InputProps) {
   const colors = useTheme();
-
   const [name, setName] = useState('');
+  const [isError, setIsError] = useState(false);
+  const { vibrate } = useVibration();
+
   const inputRef = useRef<TextInput>(null);
+
+  // Shared values for animation
+  const bounce = useSharedValue(0);
+  const rotate = useSharedValue(0);
+  const scale = useSharedValue(1);
+
   const onPress = () => {
+    setIsError(false);
     if (name.trim() === '') {
-      Alert.alert('Name is required');
+      setIsError(true);
+      vibrate();
+
+      // Trigger animations
+      bounce.value = withSpring(-5, { stiffness: 250 }, () => {
+        bounce.value = withSpring(0);
+      });
+      rotate.value = withSpring(5, { stiffness: 250 }, () => {
+        rotate.value = withSpring(-5, {}, () => {
+          rotate.value = withSpring(0);
+        });
+      });
+      scale.value = withSpring(1.1, { stiffness: 250 }, () => {
+        scale.value = withSpring(1);
+      });
+
       return;
     }
     onCall(name);
@@ -26,47 +53,65 @@ export default function Input({ onCall }: InputProps) {
   };
 
   const handleChangeText = (text: string) => {
+    setIsError(false);
     if (text.length <= 25) setName(text);
   };
 
-  return (
-    <Paper style={{ borderRadius: 50 }}>
-      <Grid align="center" row>
-        <TextInput
-          ref={inputRef}
-          onChangeText={handleChangeText}
-          value={name}
-          cursorColor={colors.accent.primary}
-          selectionColor={colors.accent.primary}
-          placeholderTextColor={colors.text.white}
-          placeholder="Enter name player"
-          onSubmitEditing={onPress}
-          style={{
-            borderWidth: 2,
-            height: '100%',
-            borderColor: colors.accent.primary,
-            paddingHorizontal: 25,
-            flex: 1,
-            borderTopLeftRadius: 50,
-            borderBottomLeftRadius: 50,
-            fontSize: 18,
-            color: colors.text.primary,
-            fontFamily: fontWeight.medium,
-            borderRightWidth: 0,
-          }}
-        />
+  const currentInputColor = isError ? colors.background.error : colors.accent.primary;
 
-        <Button
-          style={{
-            borderTopLeftRadius: 0,
-            borderBottomLeftRadius: 0,
-            paddingLeft: 15,
-          }}
-          startIcon={<FontAwesome6 name="plus" size={20} color={colors.text.primary} />}
-          onPress={onPress}
-          title="Add"
-        />
-      </Grid>
-    </Paper>
+  // Animated style for the Paper component
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{ translateY: bounce.value }, { rotate: `${rotate.value}deg` }, { scale: scale.value }],
+  }));
+
+  return (
+    <Animated.View style={animatedStyle}>
+      <Paper style={[{ borderRadius: 50 }]}>
+        <Grid align="center" row>
+          <TextInput
+            ref={inputRef}
+            onChangeText={handleChangeText}
+            value={name}
+            cursorColor={colors.accent.primary}
+            selectionColor={colors.accent.primary}
+            placeholderTextColor={isError ? colors.text.error : colors.text.white}
+            placeholder={isError ? 'Name is required' : 'Enter name player'}
+            onSubmitEditing={onPress}
+            style={{
+              borderWidth: 2,
+              height: '100%',
+              borderColor: currentInputColor,
+              paddingHorizontal: 25,
+              flex: 1,
+              borderTopLeftRadius: 50,
+              borderBottomLeftRadius: 50,
+              fontSize: 18,
+              color: isError ? colors.text.error : colors.text.primary,
+              fontFamily: fontWeight.medium,
+              borderRightWidth: 0,
+            }}
+          />
+
+          <Button
+            style={{
+              borderTopLeftRadius: 0,
+              borderBottomLeftRadius: 0,
+              paddingLeft: 15,
+              backgroundColor: currentInputColor,
+            }}
+            startIcon={<FontAwesome6 name="plus" size={20} color={colors.text.primary} />}
+            onPress={onPress}
+            title="Add"
+          />
+        </Grid>
+        {/* {isError && (
+          <Grid marginLeft={25}>
+            <Typography color="error" variant="caption-1">
+              Name is required
+            </Typography>
+          </Grid>
+        )} */}
+      </Paper>
+    </Animated.View>
   );
 }
