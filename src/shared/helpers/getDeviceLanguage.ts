bc48a06dffd06dc37f8f4597d8027837a79c5359
Ibragim <lol.3007@mail.ru>
2025-06-06 11:40:27 -0700

    Update getDeviceLanguage.ts

--- src/shared/helpers/getDeviceLanguage.ts
+++ src/shared/helpers/getDeviceLanguage.ts
@@ -2,10 +2,17 @@ import { getLocales } from 'expo-localization';
 import { DEFAULT_LANGUAGE } from '../config/constants/constants';
 import { LANGUAGE } from '../types/globalTypes';
 
+const availableLanguages: LANGUAGE[] = ['en', 'ru'];
+
 export const getDeviceLanguage = (): LANGUAGE => {
   try {
     const devLang = getLocales();
     const devLangCode = devLang[0].languageCode || DEFAULT_LANGUAGE;
+
+    if (!availableLanguages.includes(devLangCode as LANGUAGE)) {
+      return DEFAULT_LANGUAGE;
+    }
+
     return devLangCode as LANGUAGE;
   } catch {
     // errorLogger.logError('Error to get device language');
