b98e7c26503eb7a185bbaa64e66d09b7595bf55d
Ibragim <lol.3007@mail.ru>
2025-12-05 10:27:11 -0800

    translate app

--- src/shared/config/constants/languages/availableLanguages.ts
+++ src/shared/config/constants/languages/availableLanguages.ts
@@ -7,4 +7,9 @@ export interface IDisplayLanguage {
 export const languages: IDisplayLanguage[] = [
   { label: 'English', code: 'en', flag: '🇬🇧' },
   { label: 'Русский', code: 'ru', flag: '🇷🇺' },
+  { label: 'Deutsch', code: 'de', flag: '🇩🇪' },
+  { label: 'Français', code: 'fr', flag: '🇫🇷' },
+  { label: 'Español', code: 'es', flag: '🇪🇸' },
+  { label: 'Polski', code: 'pl', flag: '🇵🇱' },
+  { label: 'Filipino', code: 'fil', flag: '🇵🇭' },
 ].sort((a, b) => a.code.localeCompare(b.code));
--- src/shared/providers/i18n.ts
+++ src/shared/providers/i18n.ts
@@ -3,11 +3,21 @@ import { initReactI18next } from 'react-i18next';
 import { getDeviceLanguage } from '../helpers/getDeviceLanguage';
 import en from '@/src/locales/en.json';
 import ru from '@/src/locales/ru.json';
+import de from '@/src/locales/de.json';
+import fr from '@/src/locales/fr.json';
+import es from '@/src/locales/es.json';
+import pl from '@/src/locales/pl.json';
+import fil from '@/src/locales/fil.json';
 
-i18n.use(initReactI18next).init({
+void i18n.use(initReactI18next).init({
   resources: {
     en: { translation: en },
     ru: { translation: ru },
+    de: { translation: de },
+    fr: { translation: fr },
+    es: { translation: es },
+    pl: { translation: pl },
+    fil: { translation: fil },
   },
   lng: getDeviceLanguage(),
   fallbackLng: 'en',
